f4e204031492b9106d590c45098e0d55bb6ec134
HumptyDumpy01 <tuznikolas@gmail.com>
2024-12-29 19:28:39 +0200

    outsourced the func handler onto separate function

--- main.go
+++ main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func main() {
 	app := gin.Default()
-	app.GET("/ping", func(context *gin.Context) {
-		context.JSON(http.StatusOK, gin.H{
-			"message": "pong!",
-		})
+	app.GET("/events", getEvents)
+	err := app.Run()
+	if err != nil {
+		fmt.Println("Failed to run a server!")
+		fmt.Println(err)
+		return
+	}
+}
+
+func getEvents(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{
+		"message": "pong!",
 	})
-	app.Run()
 }
