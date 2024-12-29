399b3cdc4a359894399be1488ab54546989b4c6a
HumptyDumpy01 <tuznikolas@gmail.com>
2024-12-29 19:02:12 +0200

    Installed gin. Added the first API endpoint.

--- main.go
+++ main.go
@@ -1,4 +1,16 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
 func main() {
+	app := gin.Default()
+	app.GET("/ping", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{
+			"message": "pong!",
+		})
+	})
+	app.Run()
 }
